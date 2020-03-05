98edca8152a6ca347b17d61199bd527bdb018566
Dmitry <dimasikme@yandex.ru>
2020-03-05 12:32:49 +0300

    тестовое задание созание проекта с использованием cmake, travis и ...

--- src/cmake_train.cpp
+++ src/cmake_train.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include <math.h>
+#include <vector>
+#include <iterator>
+#include <utility>
+
 using namespace std;
 
+
 int main() {
-	cout << "!!!Hello World!!!" << endl;
-	return 0;
+	int count, answer=0;
+	bool flag=true;
+	cin>>count;
+	vector<pair<int,int> > plates;
+
+	for(int i=0;i<count;i++){
+		int one, two;
+		cin>>one>>two;
+		plates.push_back(make_pair(one,two));
+	}
+
+	for(auto start=0;start<plates.size();start++){
+		for(auto sec_start=0;sec_start<plates.size();sec_start++){
+			if( (plates[start].first < plates[sec_start].first && plates[start].second < plates[sec_start].second) ||(plates[start].first < plates[sec_start].second && plates[start].second < plates[sec_start].first)){
+				answer++;
+				break;
+			}
+		}
+	}
+
+	cout<<answer;
 }
